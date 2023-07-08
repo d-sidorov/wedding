e45fcc20f14afd5759ee007b20ad90bcf4ed56f4
d.sidorov <spens364@gmail.com>
2023-07-08 22:49:03 +0500

    com

--- src/components/UI/Button.vue
+++ src/components/UI/Button.vue
@@ -6,7 +6,7 @@ const emits = defineEmits(["click"]);
 <template>
   <button
     type="button"
-    class="text-black bg-my-primary hover:bg-my-primary-hover focus:outline-none font-medium rounded-full text-sm px-5 py-1.5 text-center disabled:bg-my-primary-disabled hover:scale-110 duration-500 transition-all cursor-pointer"
+    class="text-black bg-my-primary hover:bg-my-primary-hover focus:outline-none font-normal rounded-full text-sm px-5 py-1.5 text-center disabled:bg-my-primary-disabled hover:scale-110 duration-500 transition-all cursor-pointer"
     @click="$emit('click')"
     :disabled="disabled"
   >
